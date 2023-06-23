fa2e89a352f00caab5b2fcd975c42bd4afd43b4b
Leandro Varela Dev <71797168+leandrovarela94@users.noreply.github.com>
2023-06-23 11:28:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 class Msg(BaseModel):
     msg: str
 
